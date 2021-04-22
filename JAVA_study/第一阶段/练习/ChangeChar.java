e1b3b8cb22c28fa7b4c79db6f85e8af3be0c77aa
Chengyuhang <yuhangc525@163.com>
2021-04-22 23:34:09 +0800

    转义字符的测试

---
+++ JAVA_study/第一阶段/练习/ChangeChar.java
@@ -0,0 +1,17 @@
+
+public class ChangeChar{
+	
+	public static void main(String[] args){
+
+	System.out.println("北京\t天津\t上海");
+
+	System.out.println("jack\nsmith\nmary");
+
+	System.out.println("C:\\\\Windows\\");
+
+	System.out.println("我说:\"你要好好学习java，有前途\"");
+
+	System.out.println("123\r4");
+	
+	}
+}
