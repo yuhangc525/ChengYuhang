ef281eb23d19764a26f82b1c20d12ea670315c25
Chengyuhang <yuhangc525@163.com>
2021-04-25 11:36:54 +0800

    第一章节作业

---
+++ JAVA_study/第一阶段/chapter01/Homework02.java
@@ -0,0 +1,11 @@
+//将个人基本信息（姓名、性别、籍贯、住址）打印到控制台输出
+//各条信息分别占一行
+public class Homework02{
+	public static void main(String[] args){
+		System.out.println("姓名：程宇航");
+		System.out.println("性别：男");
+		System.out.println("籍贯:河南省开封市");
+		System.out.println("住址：陕西省西安市长安大学渭水校区");
+	}
+	
+}
